063868bdb3e34103fe94230a1833c219e0b087c0
Samuel Zhen <szhen0340@gmail.com>
2025-03-26 11:05:58 -0500

    feat: update nav color on page change

--- challenge-web-app/components/navigation.tsx
+++ challenge-web-app/components/navigation.tsx
@@ -1,32 +1,51 @@
+"use client";
+
 import { Shield } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+  const pathname = usePathname();
+
   return (
     <header className="relative z-10 mx-auto max-w-7xl px-4 py-6">
       <nav className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <Shield className="size-10 text-emerald-400" />
-          <span className="text-xl font-bold text-zinc-100">SecureSquared</span>
-        </div>
+        <Link href="/" className="text-xl font-bold text-zinc-100">
+          <div className="flex items-center gap-2">
+            <Shield className="size-10 text-emerald-400" />
+            SecureSquared
+          </div>
+        </Link>
         <div className="hidden md:flex items-center gap-6">
-          {["Features", "Solutions", "Pricing", "About"].map((item) => (
-            <a
-              key={item}
-              href="#"
-              className="text-sm text-zinc-400 hover:text-zinc-100"
-            >
-              {item}
-            </a>
-          ))}
+          {["FinanceGuard", "HealthSync", "SecureHome", "Careers", "Blog"].map(
+            (item) => (
+              <a
+                key={item}
+                href={"/" + item.toLowerCase()}
+                className={`text-sm ${
+                  "/" + item.toLowerCase() === pathname
+                    ? "text-emerald-400"
+                    : "text-zinc-400 hover:text-zinc-100"
+                }`}
+              >
+                {item}
+              </a>
+            )
+          )}
         </div>
         <div className="flex items-center gap-4">
-          <Button variant="ghost" className="text-zinc-400 hover:text-zinc-100">
-            Sign In
-          </Button>
-          <Button className="bg-zinc-100 text-zinc-900 hover:bg-zinc-200">
-            Get Started
-          </Button>
+          <Link href="/signin">
+            <Button
+              variant="ghost"
+              className="text-zinc-400 hover:text-zinc-100 hover:cursor-pointer"
+            >
+              Sign In
+            </Button>
+            <Button className="bg-zinc-100 text-zinc-900 hover:bg-zinc-300 hover:cursor-pointer">
+              Get Started
+            </Button>
+          </Link>
         </div>
       </nav>
     </header>
