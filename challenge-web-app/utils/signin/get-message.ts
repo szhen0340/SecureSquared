76f0b0ce3332fa83990f52dda3051e7846b32b45
szhen0340 <szhen0340@gmail.com>
2025-04-07 00:09:10 -0500

    fix: server action must be async

--- challenge-web-app/utils/signin/get-message.ts
+++ challenge-web-app/utils/signin/get-message.ts
@@ -1,6 +1,6 @@
 "use server";
 
-export const getMessage = (similarity: number) => {
+export const getMessage = async (similarity: number) => {
   return similarity > 0.6
     ? "Your identity has been verified. The flag is flame{3mb3dd1ngs_4r3_gr8!}."
     : "Your identity could not be verified. Please try again.";
